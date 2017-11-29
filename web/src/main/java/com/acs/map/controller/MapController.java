b3629244382ca0654d9284f850341b611066a338
Yurii Piets <yurii.piets@gmail.com>
2017-11-30 00:28:51 +0100

    html is returned by controller with all js and css files

--- web/src/main/java/com/acs/map/controller/MapController.java
+++ web/src/main/java/com/acs/map/controller/MapController.java
@@ -5,11 +5,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
-// TODO: 29/11/2017 doesn't work, returns `PageNotFound: Request method 'GET' not supported
 public class MapController {
 
     @RequestMapping(value = "/", method = RequestMethod.GET)
     public String index() {
-        return "google/map.html";
+        return "map.html";
     }
 }
