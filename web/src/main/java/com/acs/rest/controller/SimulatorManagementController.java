4e7e065cb646469cd6925e22526abf01e1c1457c
Yurii Piets <yurii.piets@gmail.com>
2018-01-11 18:52:52 +0100

    removed comments from SimulatorManagementController

--- web/src/main/java/com/acs/rest/controller/SimulatorManagementController.java
+++ web/src/main/java/com/acs/rest/controller/SimulatorManagementController.java
@@ -1,42 +1,48 @@
 package com.acs.rest.controller;
 
+import com.acs.simulator.def.AgentSimulator;
+import com.acs.simulator.def.LightsSimulator;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping(value = "/simulator")
 public class SimulatorManagementController {
-//
-//    private final AgentSimulator agentSimulator;
-//
-//    private final LightsSimulator lightsSimulator;
-//
-//    @Autowired
-//    public SimulatorManagementController(AgentSimulator agentSimulator,
-//                                         LightsSimulator lightsSimulator) {
-//        this.agentSimulator = agentSimulator;
-//        this.lightsSimulator = lightsSimulator;
-//    }
-//
-//    @RequestMapping(method = RequestMethod.PUT, value = "/reset")
-//    public ResponseEntity<?> reset() {
-//        agentSimulator.resetSimulation();
-//        lightsSimulator.resetSimulation();
-//
-//        return new ResponseEntity(HttpStatus.OK);
-//    }
-//
-//    @RequestMapping(method = RequestMethod.PUT, value = "/agents/reset")
-//    public ResponseEntity<?> resetAgentsSimulation() {
-//        agentSimulator.resetSimulation();
-//
-//        return new ResponseEntity(HttpStatus.OK);
-//    }
-//
-//    @RequestMapping(method = RequestMethod.PUT, value = "/lights/reset")
-//    public ResponseEntity<?> resetLightsSimulations() {
-//        lightsSimulator.resetSimulation();
-//
-//        return new ResponseEntity(HttpStatus.OK);
-//    }
+
+    private final AgentSimulator agentSimulator;
+
+    private final LightsSimulator lightsSimulator;
+
+    @Autowired
+    public SimulatorManagementController(AgentSimulator agentSimulator,
+                                         LightsSimulator lightsSimulator) {
+        this.agentSimulator = agentSimulator;
+        this.lightsSimulator = lightsSimulator;
+    }
+
+    @RequestMapping(method = RequestMethod.PUT, value = "/reset")
+    public ResponseEntity<?> reset() {
+        agentSimulator.resetSimulation();
+        lightsSimulator.resetSimulation();
+
+        return new ResponseEntity(HttpStatus.OK);
+    }
+
+    @RequestMapping(method = RequestMethod.PUT, value = "/agents/reset")
+    public ResponseEntity<?> resetAgentsSimulation() {
+        agentSimulator.resetSimulation();
+
+        return new ResponseEntity(HttpStatus.OK);
+    }
+
+    @RequestMapping(method = RequestMethod.PUT, value = "/lights/reset")
+    public ResponseEntity<?> resetLightsSimulations() {
+        lightsSimulator.resetSimulation();
+
+        return new ResponseEntity(HttpStatus.OK);
+    }
 }
